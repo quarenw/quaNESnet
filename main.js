2afa660d4579e30d776fba108699aff19f4a04ed
quaren <quaren@gmail.com>
2021-11-28 12:56:40 +0000

    Web presentation

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const canvas = document.querySelector('#canvas canvas')
 console.log(canvas)
-const display = new Display(canvas)
 let running = false 
-let frame = 0
 let oldTime = window.performance.now()
+let oldFrame = 0
 let fpsEle = document.querySelector('#fps')
+let selectedRom = './roms/sgt.nes'
+let emulation = {}
 
 const joyMapping = {
   '87': { inputKey: 'w', gamepadKey: 'up'},
@@ -15,6 +16,121 @@ const joyMapping = {
   '190': { inputKey: ',', gamepadKey: 'b'},
   '188': { inputKey: '.', gamepadKey: 'a'},
   '191': { inputKey: '/', gamepadKey: 'select'}
+}
+
+settings()
+runCartridge()
+
+function runCartridge () {
+  const url = selectedRom
+  if (url === null) {
+    const fileInput = document.querySelector('#fileInput')
+    fileInput.addEventListener('change', e => {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.onload = e => handleLoad(e.target.result)
+      reader.readAsArrayBuffer(file)
+    }, false)
+    fileInput.click()
+  } else {
+    const request = new XMLHttpRequest()
+    request.responseType = 'arraybuffer'
+    request.onload = () => handleLoad(request.response)
+    request.open('GET', url, true)
+    request.send(null)
+  }
+
+  function handleLoad (response) {
+    const buffer = response
+
+    const display = new Display(canvas)
+    const nes = new Bus()
+    const cart = new Cartridge(buffer)
+    if (!cart.imageValid()) return false 
+    emulation.display = display
+    emulation.nes = nes
+    emulation.cart = cart
+    nes.insertCartridge(cart)
+    nes.attachDisplay(display)
+    nes.reset()
+    
+    window.onkeydown = e => {
+      if (e.keyCode == 32) {
+        running = !running
+        document.querySelector('#status').textContent = running ? 'Status: Running' : 'Status: Not running'
+        run()
+      }
+      
+      const button = joyMapping[e.keyCode]
+      if (button) {
+        nes.joypad[button.gamepadKey] = 1
+      }
+  
+      e.preventDefault()
+    }
+  
+    window.onkeyup = e => {
+      const button = joyMapping[e.keyCode]
+      if (button) {
+        nes.joypad[button.gamepadKey] = 0
+      }
+  
+      e.preventDefault()
+    }
+  
+    function run () {
+      const cycles = (341 * 262 /3) | 0
+  
+      nes.controller[0][0] = 0x00
+      nes.controller[0][0] |= nes.joypad['a'] ? 0x80 : 0x00
+      nes.controller[0][0] |= nes.joypad['b'] ? 0x40 : 0x00
+      nes.controller[0][0] |= nes.joypad['select'] ? 0x20 : 0x00
+      nes.controller[0][0] |= nes.joypad['start'] ? 0x10 : 0x00
+      nes.controller[0][0] |= nes.joypad['up'] ? 0x08 : 0x00
+      nes.controller[0][0] |= nes.joypad['down'] ? 0x04 : 0x00
+      nes.controller[0][0] |= nes.joypad['left'] ? 0x02 : 0x00
+      nes.controller[0][0] |= nes.joypad['right'] ? 0x01 : 0x00
+  
+      for (let i = 0; i < cycles; i ++) nes.clock()
+      if (running) {
+        if (oldFrame !== ppu.frame) measureFps()
+        window.requestAnimationFrame(run)
+      }
+    }
+  
+    function measureFps () {
+      const newTime = window.performance.now()
+      if (ppu.frame % 20 === 0) {
+        let fps =  1000 / ((newTime - oldTime) / (ppu.frame - oldFrame))
+        fpsEle.innerHTML = `fps: ${fps.toFixed(0)}`
+        oldFrame = ppu.frame
+        oldTime = newTime
+      }
+    }
+  
+  
+  }
+}
+
+function settings () {
+  const scanlinesInput = document.querySelector('#scanlines')
+  const scanlinesImg = document.querySelector('#canvas img')
+
+  scanlines.addEventListener('change', (e) => {
+    scanlinesImg.classList.toggle('hide') 
+  })
+
+  const romSelectors = document.querySelectorAll('#romSelection div.line')
+  romSelectors.forEach(i => i.addEventListener('click', e => {
+    selectedRom = e.target.getAttribute('data-option')
+    running = false
+    delete emulation.nes
+    delete emulation.display
+    delete emulation.cart
+    document.querySelector('#status').textContent = "Status: Not running"
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    runCartridge()
+  }))
 }
 
 function printMem (mem) {
@@ -47,77 +163,3 @@ function hex (num, width = 2, noPrefix) {
   return prefix + (base + str).substr(-1 * width)
 }
 
-const url = './smb.nes'
-const request = new XMLHttpRequest()
-request.responseType = 'arraybuffer'
-
-request.onload = () => {
-  var buffer = request.response
-  const nes = new Bus()
-  const cart = new Cartridge(buffer)
-  if (!cart.imageValid()) return false 
-  nes.insertCartridge(cart)
-  nes.attachDisplay(display)
-  nes.reset()
-  
-  window.onkeydown = e => {
-    if (e.keyCode == 32) {
-      running = !running
-      run()
-    }
-    
-    const button = joyMapping[e.keyCode]
-    if (button) {
-      nes.joypad[button.gamepadKey] = 1
-    }
-
-    e.preventDefault()
-  }
-
-  window.onkeyup = e => {
-    const button = joyMapping[e.keyCode]
-    if (button) {
-      nes.joypad[button.gamepadKey] = 0
-    }
-
-    e.preventDefault()
-  }
-
-  function run () {
-    const cycles = (341 * 262 /3) | 0
-
-    nes.controller[0][0] = 0x00
-    nes.controller[0][0] |= nes.joypad['a'] ? 0x80 : 0x00
-    nes.controller[0][0] |= nes.joypad['b'] ? 0x40 : 0x00
-    nes.controller[0][0] |= nes.joypad['select'] ? 0x20 : 0x00
-    nes.controller[0][0] |= nes.joypad['start'] ? 0x10 : 0x00
-    nes.controller[0][0] |= nes.joypad['up'] ? 0x08 : 0x00
-    nes.controller[0][0] |= nes.joypad['down'] ? 0x04 : 0x00
-    nes.controller[0][0] |= nes.joypad['left'] ? 0x02 : 0x00
-    nes.controller[0][0] |= nes.joypad['right'] ? 0x01 : 0x00
-
-    for (let i = 0; i < cycles; i ++) nes.clock()
-    if (running) {
-      measureFps()
-      window.requestAnimationFrame(run)
-    }
-  }
-
-  function measureFps () {
-    if (frame === 60) {
-      const newTime = window.performance.now()
-      if (oldTime !== null) {
-        let fps = 60000 / (newTime - oldTime)
-        fpsEle.innerHTML = `fps: ${fps.toFixed(2)}`
-      }
-      oldTime = newTime
-      frame = 0
-    }
-    frame++
-  }
-
-
-}
-
-request.open('GET', url, true)
-request.send(null)
